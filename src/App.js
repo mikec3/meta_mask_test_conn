fe489f90957e1069c02d59f35059d60f7bda183d
Michael Couch <michaelcouch@MacBook-Pro.local>
2021-09-05 14:59:12 -0700

    added metaMask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3'
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [userAccount, setUserAccount] = useState(null);
+
+if (window.ethereum) {
+  console.log(window.ethereum);
+  window.web3 = new Web3(window.ethereum);
+  console.log(window.web3.eth.getAccounts());
+
+  window.ethereum.send('eth_requestAccounts')
+  .then(function(result){
+    console.log(result.result[0]);
+    setUserAccount(result.result[0]);
+  })
+
+} else {
+  alert("You must install MetaMask to interact with this site");
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <p> account: {userAccount}</p>
     </div>
   );
 }
